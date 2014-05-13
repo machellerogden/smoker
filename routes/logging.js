a7131e761cfc1eb2dc2b828f8a311b6e9bd8744c
Kevin Phillips <kphillips@cars.com>
2014-05-13 12:08:14 -0500

    fixing logger

--- routes/logging.js
+++ routes/logging.js
@@ -1,6 +1,8 @@
 module.exports = {
     create: function (req, res) {
-        console.log(req.data);
+        console.log('==========LOGGER==========');
+        console.log(req.body);
+        console.log('========END LOGGER========');
         res.json({status: 'Nice Job!'});
     }
 };
